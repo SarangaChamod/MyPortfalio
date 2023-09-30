3da860161b73231d6d6513780fcc60940c4f862f
Saranga Chamod <91281208+SarangaChamod@users.noreply.github.com>
2023-09-30 16:57:38 +0530

    Navbar updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,7 +12,7 @@ const links = [
         active: 'home'
     },
     {
-        name: 'About Me',
+        name: 'About_Me',
         to: '/about',
         active: 'about'
     },
@@ -28,7 +28,7 @@ const links = [
         active: 'portfolio'
     },
     {
-        name: 'ui desigs',
+        name: 'ui_desigs',
         to: '/uiDesign',
         active: 'uiDesign'
     }
